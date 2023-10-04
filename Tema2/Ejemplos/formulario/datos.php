1b9efc5d7c2fbe075b368c2bd22ceeb908dccd2d
Clase <>
2023-10-04 09:29:54 +0200

    formulario PINO

---
+++ Tema2/Ejemplos/formulario/datos.php
@@ -0,0 +1,12 @@
+<?php
+
+if (isset($_POST['enviar'])) {
+    //var_dump($_POST);
+    echo "<br>Nombre: " . $_POST["nombre"];
+    echo "<br>Apellidos: " .$_POST["apell"];
+    echo "<br>Nº Matricula: " .$_POST["matricula"];
+    echo "<br>Curso: " .$_POST["curso"];
+    echo "<br>Precio: " .$_POST["precio"];
+    echo '<br><a href="index.php">Atras</a>';
+}
+?>
---
+++ Tema2/Ejemplos/formulario/index.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>title</title>
+    </head>
+    <body>
+        <form action="Matricula.php" method="POST">
+            Nombre: <input type="text" name="nombre"><br>
+            Apellidos: <input type="text" name="apell"><br>
+            <input type="submit" name="enviar" value="Enviar">
+        </form>
+    </body>
+</html>
